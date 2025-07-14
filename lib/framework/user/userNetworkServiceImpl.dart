a829e360aac0118320362a0b43029bf6947665cc
impulse <ephraimimpulse17@gmail.com>
2025-07-14 11:26:44 +0100

    implementation de la methode registerUser et verifyOtp dans le userNetworkServiceImpl

--- lib/framework/user/userNetworkServiceImpl.dart
+++ lib/framework/user/userNetworkServiceImpl.dart
@@ -1,10 +1,15 @@
 
+import 'package:odc_mobile_template/business/models/user/registerUser.dart';
+
+import 'package:odc_mobile_template/business/models/user/verifyOtp.dart';
+
 import '../../business/models/user/authentication.dart';
 
 import '../../business/models/user/user.dart';
 
 import '../../business/services/user/userNetworkService.dart';
 import '../../utils/http/HttpUtils.dart';
+import '../utils/http/localHttpUtils.dart';
 
 class UserNetworkServiceImpl extends UserNetworkService {
   final String baseUrl;
@@ -24,5 +29,50 @@ class UserNetworkServiceImpl extends UserNetworkService {
     throw UnimplementedError();
   }
 
-  
+  @override
+  Future<void> registerUser(RegisterUser registerUser) async {
+   var url = '$baseUrl/register/';
+   var body = registerUser.toJson();
+   var response = await httpUtils.postData(url,body: body);
+   print(response);
+   return;
+  }
+
+  @override
+  Future<void> resendOtp(VerifyOtp resendOtp) {
+    // TODO: implement resendOtp
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> verifyOtp(VerifyOtp verifyOtp) async {
+    var url = '$baseUrl/verify-otp';
+    var body = verifyOtp.toJson();
+    var response = await httpUtils.postData(url, body : body);
+    print(response);
+    return ;
+
+  }
+
+
+
+
+  }
+void main() async {
+  //test register
+  var service = UserNetworkServiceImpl(
+    baseUrl: "http://10.252.252.61:8000/api",
+    httpUtils: LocalHttpUtils(),
+  );
+  try{
+    var data=VerifyOtp(email:'email@gmail.com',otp: '170400');
+    var r=await service.verifyOtp(data);
+    print(data);
+
+  }catch(e, s){
+    print(e);
+    print(s);
+  }
+
+
 }

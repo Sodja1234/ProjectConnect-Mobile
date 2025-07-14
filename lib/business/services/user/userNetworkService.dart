af2ec40f7882c2982b1dd16e648ec303ff7f3bbb
impulse <ephraimimpulse17@gmail.com>
2025-07-14 11:24:20 +0100

    création des methode abstraite registerUser,verifyOtp,resendOtp dans le userLocalService

--- lib/business/services/user/userNetworkService.dart
+++ lib/business/services/user/userNetworkService.dart
@@ -1,7 +1,13 @@
+import 'package:odc_mobile_template/business/models/user/registerUser.dart';
+import 'package:odc_mobile_template/business/models/user/verifyOtp.dart';
+
 import '../../models/user/authentication.dart';
 import '../../models/user/user.dart';
 
 abstract class UserNetworkService {
   Future<User> seConnecter(Authentication authentication);
   Future<User> recupererInfoUtilisateur();
+  Future<void> registerUser(RegisterUser registerUser);
+  Future<void> verifyOtp(VerifyOtp verifyOtp);
+  Future<void> resendOtp(VerifyOtp resendOtp);
 }
